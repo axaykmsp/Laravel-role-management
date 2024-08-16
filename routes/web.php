d1026ad1e79059f910229811959a9bf179bfd875
axaykmsp <axay.k@msp-group.co.uk>
2024-08-16 13:06:50 +0530

    stripe integration'

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\ProfileController;
+use App\Http\Controllers\StripeController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
@@ -16,6 +17,10 @@ Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+    Route::get('/charge', [StripeController::class, 'index'])->name('stripe.charge');
+    Route::post('/charge', [StripeController::class, 'add'])->name('stripe.charge.create');
+    Route::get('/success', [StripeController::class, 'success'])->name('stripe.success');
+    Route::get('/cancel', [StripeController::class, 'cancel'])->name('stripe.cancel');
 });
 
 require __DIR__.'/auth.php';
